f31a687ec5a16847bf38d7faafcebe576858a18d
advait1109 <86603536+advait1109@users.noreply.github.com>
2022-07-29 16:34:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -7,10 +11,28 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
 }
 function play(){
     song.play();
+    song.rate(1);
+    song.setVolume(1);
+}
+function modelLoaded(){
+    console.log("MODEL LOADED");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log(leftWristX+" "+leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log(rightWristX+" "+rightWristY);
+    }
 }
